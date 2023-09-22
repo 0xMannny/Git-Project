6a99d77987917edf6ece68205035a9e735bc3ad2
Luke Collins <luke23571113@gmail.com>
2023-09-22 01:01:27 -0700

    added createTree test which is expected to fail

---
+++ CommitTest.java
@@ -0,0 +1,72 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.*;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedHashMap;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CommitTest {
+
+    private static String testFile = "testfile.txt";
+
+    @BeforeAll
+    static void runBeforeAll () throws Exception {
+        //make a test file
+        File f = new File(testFile);
+        f.createNewFile();
+
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(testFile)));
+
+        pw.print("foobar"); 
+        pw.close();
+
+        //make objects file
+        File objects = new File ("./objects");
+        objects.mkdirs();
+
+    }
+
+
+    //NOTE: this won't work right now because createTree just creates an empty tree
+    @Test
+    void testCreateTree() throws Exception {
+        Tree t = new Tree();
+        t.addWithToObjects(testFile);
+
+        String correctHash = t.save();
+
+        Commit c = new Commit ("luke", "bleh");
+        String commitHash = c.createTree();
+
+        assertTrue("the commit hash did not match the tree hash", correctHash.equals(commitHash));
+    }
+
+    @Test
+    void testGetDate() {
+
+    }
+
+    @Test
+    void testGetSHA1() {
+
+    }
+
+    @Test
+    void testWriteToFile() {
+
+    }
+}
